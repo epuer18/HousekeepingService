0f2088755dd3fb0c8f4971aca9d0aa03415328f0
Vickidiaz <vickidiaz42@gmail.com>
2023-10-09 05:01:42 -0700

    moved files, added landing page, add posting feature working

---
+++ scripts/postPage.js
@@ -0,0 +1,40 @@
+// Connect an event listener to the button "showFormBtn"
+// When this button is clicked, the provided function will be executed
+document.getElementById("showFormBtn").addEventListener("click", function () {
+  document.getElementById("formContainer").style.display = "block";
+});
+// Fetch the element with id "formContainer" and set its CSS display property to "block",
+// which means it will be shown on the page (if it was previously hidden)
+// get the value from the input field with id "userPhone"
+
+document.getElementById("submitBtn").addEventListener("click", function () {
+  const userName = document.getElementById("userName").value;
+  const userPhone = document.getElementById("userPhone").value;
+  const workLocation = document.getElementById("workLocation").value;
+  const dayAvailable = document.getElementById("dayAvailable").value;
+  const timeAvailable = document.getElementById("timeAvailable").value;
+  const rateHour = document.getElementById("rateHour").value;
+  const cleaningServices = document.getElementById("cleaningServices").value;
+
+  const card = document.createElement("div");
+  card.className = "card col-md-3 mb-4"; // Added col-md-3 and mb-4 for margin
+  // what will be displayed on card
+  card.innerHTML = `
+    <div class="card-body">  <!-- Use Bootstrap's card-body class for padding -->
+        <p>Name: ${userName}</p>
+        <p>Phone: ${userPhone}</p>
+        <p>Location Open To Work At: ${workLocation}</p>
+        <p>Day available ($/hr): ${dayAvailable}</p>
+        <p>Time Available ($/hr): ${timeAvailable}</p>
+        <p>Rate ($/hr): ${rateHour}</p>
+        <p>Cleaning Services: ${cleaningServices}</p>
+    </div>
+`;
+  // add carrd to div
+  document.getElementById("cardsContainer").appendChild(card);
+
+  // Hide the form
+  // how? set its CSS display property to "none"
+
+  document.getElementById("formContainer").style.display = "none";
+});
